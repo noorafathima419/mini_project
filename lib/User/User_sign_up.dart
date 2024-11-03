1c4a3f5114bf3cd71db056b2f1fc6916b01983c1
HP <noorafathimafeb26@gmail.com>
2024-11-03 21:31:20 +0530

    03/11/2024

---
+++ lib/User/User_sign_up.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class UserSignUp extends StatefulWidget {
+  const UserSignUp({super.key});
+
+  @override
+  State<UserSignUp> createState() => _UserSignUpState();
+}
+
+class _UserSignUpState extends State<UserSignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(backgroundColor: Color(0xffCFE2FF),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30.h, left: 10.w),
+                child: Container(
+                  child: Icon(Icons.arrow_back_ios_sharp),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 100.w, ),
+                child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/admin.png"))),
+                  height: 200.h,
+                  width: 200.w,
+                  //
+                  // color: Colors.grey,
+                ),
+              )
+            ],
+          ),Row(
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 150.w,),
+                  child: Text(
+                    "Sign up",
+                    style:
+                    TextStyle(fontWeight: FontWeight.w700, fontSize: 28.sp),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 70.w,top: 50.h),
+                  child: Text(
+                    "Enter username",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 70.w),
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 15.h, left: 20.w),
+                    child: Text(
+                      "username",
+                      style: GoogleFonts.poppins(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey),
+                    ),
+                  ),
+                  height: 50.h,
+                  width: 286.w,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.r),
+                      color: Colors.white),
+                ),
+              )
+            ],
+          ),Row(
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 70.w,top: 20.h),
+                  child: Text(
+                    "Enter phonenumber",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 70.w),
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 15.h, left: 20.w),
+                    child: Text(
+                      "Phone number",
+                      style: GoogleFonts.poppins(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey),
+                    ),
+                  ),
+                  height: 50.h,
+                  width: 286.w,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.r),
+                      color: Colors.white),
+                ),
+              )
+            ],
+          ),Row(
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 70.w,top: 20.h),
+                  child: Text(
+                    "Enter your email",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 70.w),
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 15.h, left: 20.w),
+                    child: Text(
+                      "enter email",
+                      style: GoogleFonts.poppins(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey),
+                    ),
+                  ),
+                  height: 50.h,
+                  width: 286.w,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.r),
+                      color: Colors.white),
+                ),
+              )
+            ],
+          ),Row(
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 70.w,top: 20.h),
+                  child: Text(
+                    "Enter your password",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 70.w,),
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 15.h, left: 20.w),
+                    child: Text(
+                      "enter your password",
+                      style: GoogleFonts.poppins(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey),
+                    ),
+                  ),
+                  height: 50.h,
+                  width: 286.w,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.r),
+                      color: Colors.white),
+                ),
+              )
+            ],
+          ),Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 120.w, top: 100.h),
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      "SIGN UP",
+                      style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20.sp),
+                    ),
+                  ),
+                  height: 50.h,
+                  width: 200.w,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.r),
+                      color: Color(0xff2357D9)),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
