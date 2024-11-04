7e3e57fe5e34317209cf5a3b4e04162789067840
HP <noorafathimafeb26@gmail.com>
2024-11-04 11:44:17 +0530

    04/11/2024

---
+++ lib/Mechanic/Mech_notification.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MechNotification extends StatefulWidget {
+  const MechNotification({super.key});
+
+  @override
+  State<MechNotification> createState() => _MechNotificationState();
+}
+
+class _MechNotificationState extends State<MechNotification> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffCFE2FF),
+        leading: Icon(Icons.arrow_back_ios_sharp),
+        title: Padding(
+          padding: EdgeInsets.only(left: 80.w),
+          child: Text("Notification"),
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 50.h, left: 50.w),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.w, top: 10.h),
+                            child: Text(
+                              "Admin Notification",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.grey, fontSize: 18.sp),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10.h, left: 50.w),
+                            child: Text(
+                              "10:00 am",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 18.sp, fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 50.h, left: 190.w),
+                        child: Row(
+                          children: [
+                            Text(
+                              "10/05/2023",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 18.sp, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  width: 300.w,
+                  height: 123.h,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.r),
+                      color: Colors.white,
+                      border: Border.all(width: 1.w, color: Colors.black)),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
