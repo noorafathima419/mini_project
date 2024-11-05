e0cbb378428b8c08b8cfb3c90cfde272441e5c8d
HP <noorafathimafeb26@gmail.com>
2024-11-05 20:14:57 +0530

    05/11/2024

---
+++ lib/Mechanic/Mech_status_reject.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MechStatusReject extends StatefulWidget {
+  const MechStatusReject({super.key});
+
+  @override
+  State<MechStatusReject> createState() => _MechStatusRejectState();
+}
+
+class _MechStatusRejectState extends State<MechStatusReject> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
